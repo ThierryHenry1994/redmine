a3eb68100f604a92314194e7e11cdf8aa9a0594c
ThierryHenry1994 <32047768+ThierryHenry1994@users.noreply.github.com>
2021-03-31 22:12:56 +0800

    update:excel has been done

--- redmine_conntect.py
+++ redmine_conntect.py
@@ -1,7 +1,23 @@
-
-
 from redminelib import Redmine
 import xlsxwriter
+
+
+def handle_user_info(user_info):
+    result = []
+    for users in user_info:
+        user_variety = users[0]
+        user_name = users[1]
+        old_variety = ""
+        for s in result:
+            if user_variety == s[0]:
+                s[1].append(user_name)
+                old_variety = True
+            else:
+                pass
+        if not old_variety:
+            result.append([user_variety, [user_name]])
+
+    return result
 
 
 def get_member_from_redmine(redmine_url, redmine_key):
@@ -19,21 +35,33 @@ def get_member_from_redmine(redmine_url, redmine_key):
             if "user" in value.keys():
                 group = value["roles"][0]["name"]
                 name = value["user"]["name"]
-                _dict = {group: name}
-                user_list.append(_dict)
+                user_list.append((group, name))
+                # print(user_list)
+        user_list = handle_user_info(user_list)
         project_data["member_info"] = user_list
         project_list.append(project_data)
-    print(project_list)
+    return project_list
 
 
-def write_excel():
-    workbook = xlsxwriter.Workbook("./test.xlsx")
-    worksheet = workbook.add_worksheet()
-    worksheet.write(0, 0, "project_name")
-    workbook.close()
+def write_excel(excel_list):
+    for sub in range(len(excel_list)):
+
+        excel_name = "./"+excel_list[sub]["name"]+".xlsx"
+        workbook = xlsxwriter.Workbook(excel_name)
+        worksheet = workbook.add_worksheet()
+        worksheet.write(0, 0, "project_name")
+        worksheet.write(0, 1, "member")
+        count = 0
+        for info in excel_list[sub]["member_info"]:
+
+            print("======", info)
+            worksheet.write(count+1, 0, info[0])
+            worksheet.write(count + 1, 1, ','.join(info[1]))
+            count +=1
+        workbook.close()
 
 
 url = 'http://demo.redmineup.com'
 key = "9a13f31770b80767a57d753961acbd3a18eb1370"
-get_member_from_redmine(url, key)
-# write_excel()
+p_list = get_member_from_redmine(url, key)
+write_excel(p_list)
